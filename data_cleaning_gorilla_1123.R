85dd6a8249521c20ae0cbbec1415681cf2c4f648
Katie Moran <64905014+katielmoran@users.noreply.github.com>
2023-12-05 13:08:57 +0000

    Minor updates to condense code. Added in write.csv

--- data_cleaning_gorilla_1123.R
+++ data_cleaning_gorilla_1123.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 #Set working directory
-setwd("Z:/Biobank_Vascular/Windows/Gorilla_Mark_1123/shortform_unblind_mark")
+setwd("Z:/Biobank_Vascular/Windows/Gorilla_Mark_1123/shortform_unblind_wex")
 
 #reading in data and renaming according to task-type
 visual_ps <- read.csv("data_exp_71706-v1_task-fbg8.csv")
@@ -24,18 +24,19 @@ visual_exp <- visual_exp[-c(1:11, 13:14, 16:36, 39:40, 45:47, 49:50) ]
 
 #visually checked data to ensure "correct" answers are actually correct - which they are!
 
-#if the screen timed out, changing the value from incorrect to NA
+#if the screen timed out, changing the values to NA
 visual_exp$Incorrect[visual_exp$Timed.Out == 1] <- NA
 visual_exp$Correct[visual_exp$Timed.Out == 1] <- NA
+visual_exp$Reaction.Time[visual_exp$Timed.Out == 1] <- NA
 
-#condensing data down to tabular form - one ppt per row
-visual_condensed <- visual_exp[-c(2:3, 5, 9:13) ]
+#changing RT from character to numeric
+visual_exp$Reaction.Time <- as.numeric(visual_exp$Reaction.Time)
 
-visual_sums <- visual_condensed %>% group_by(Participant.Public.ID) %>% 
-  summarise(vis_mean_ps = mean(Reaction.Time), vis_total_attempt = sum(Attempt, na.rm = TRUE), 
+#condensing down to total attempted, total correct, total incorrect and mean reaction time 
+visual_sums <- visual_exp %>% group_by(Participant.Public.ID) %>% 
+  summarise(vis_mean_ps = mean(Reaction.Time, na.rm = TRUE), vis_total_attempt = sum(Attempt, na.rm = TRUE), 
             vis_total_correct = sum(Correct, na.rm = TRUE),
             vis_total_incorrect = sum(Incorrect, na.rm = TRUE))
-
 
 #cleaning semantic processing speed task
 
@@ -48,17 +49,16 @@ semantic_exp <- semantic_exp[!grepl("fixation", semantic_exp$Zone.Name),]
 #deleting columns that aren't useful
 semantic_exp <- semantic_exp[-c(1:11, 13:14, 16:36, 39:40, 45:47, 49:50) ]
 
-#if the screen timed out, changing the value from incorrect to NA
+#if the screen timed out, changing the value to NA
 semantic_exp$Incorrect[semantic_exp$Timed.Out == 1] <- NA
 semantic_exp$Correct[semantic_exp$Timed.Out == 1] <- NA
+semantic_exp$Reaction.Time[semantic_exp$Timed.Out == 1] <- NA
 
 #visually checked data to ensure "correct" answers are actually correct - which they are!
 
-#condensing data down to relevant columns
-semantic_condensed <- semantic_exp[-c(2:3, 5, 9:13) ]
-
-semantic_sums <- semantic_condensed %>% group_by(Participant.Public.ID) %>% 
-  summarise(sem_mean_ps = mean(Reaction.Time), sem_total_attempt = sum(Attempt, na.rm = TRUE), 
+#condensing down to total attempted, total correct, total incorrect and mean reaction time 
+semantic_sums <- semantic_exp %>% group_by(Participant.Public.ID) %>% 
+  summarise(sem_mean_ps = mean(Reaction.Time, na.rm = TRUE), sem_total_attempt = sum(Attempt, na.rm = TRUE), 
             sem_total_correct = sum(Correct, na.rm = TRUE),
             sem_total_incorrect = sum(Incorrect, na.rm = TRUE))
 
@@ -83,12 +83,11 @@ motor_exp <- motor_exp %>% rename(Correct = "temp")
 #correcting for timed out trials, where they were coded as incorrect 
 motor_exp$Incorrect[motor_exp$Timed.Out == 1] <- NA
 motor_exp$Correct[motor_exp$Timed.Out == 1] <- NA
+motor_exp$Reaction.Time[motor_exp$Timed.Out == 1] <- NA
 
-#condensing data down to relevant columns
-motor_condensed <- motor_exp[-c(2:3, 5, 9:13) ]
-
-motor_sums <- motor_condensed %>% group_by(Participant.Public.ID) %>% 
-  summarise(motor_mean_ps = mean(Reaction.Time), motor_total_attempt = sum(Attempt, na.rm = TRUE), 
+#creating correct, incorrect and attempted totals, as well as average reaction times 
+motor_sums <- motor_exp %>% group_by(Participant.Public.ID) %>% 
+  summarise(motor_mean_ps = mean(Reaction.Time, na.rm = TRUE), motor_total_attempt = sum(Attempt, na.rm = TRUE), 
             motor_total_correct = sum(Correct, na.rm = TRUE),
             motor_total_incorrect = sum(Incorrect, na.rm = TRUE))
 
@@ -103,9 +102,10 @@ basic_rt_exp <- basic_rt_exp[!grepl("fixation", basic_rt_exp$Zone.Name),]
 #deleting columns that aren't useful
 basic_rt_exp <- basic_rt_exp[-c(1:11, 13:14, 16:36, 39:40, 45:47, 49:50) ]
 
-#correcting for timed out trials, where they were coded as incorrect 
+#correcting for timed out trials, where they were coded as incorrect to NA
 basic_rt_exp$Incorrect[basic_rt_exp$Timed.Out == 1] <- NA
 basic_rt_exp$Correct[basic_rt_exp$Timed.Out == 1] <- NA
+basic_rt_exp$Reaction.Time[basic_rt_exp$Timed.Out == 1] <- NA
 
 #creating a correct rejection column - ppt doesn't respond to incorrect stimuli
 basic_rt_exp <- mutate(basic_rt_exp, correct_rejection = ifelse(Timed.Out == 1 & is.na(Response), 1, 0))
@@ -121,14 +121,37 @@ basic_rt_exp$false_alarm <- ifelse(is.na(basic_rt_exp$false_alarm), 0, basic_rt_
 #creating a miss column - ppt doesn't respond to correct stimuli
 basic_rt_exp <- mutate(basic_rt_exp, miss = ifelse(is.na(Response) & Letters == "H.png", 1, 0))
 
-#condensing data down to relevant columns
-basic_rt_condensed <- basic_rt_exp[-c(2:3, 5, 9:12) ]
-
-
-basic_rt_sums <- basic_rt_condensed %>% group_by(Participant.Public.ID) %>% 
-  summarise(brt_mean_ps = mean(Reaction.Time), 
+#creating a total correct hits, false alarms, misses and correct rejection sums and average reaction time
+basic_rt_sums <- basic_rt_exp %>% group_by(Participant.Public.ID) %>% 
+  summarise(brt_mean_ps = mean(Reaction.Time, na.rm = TRUE), 
             brt_total_correct_hits = sum(correct_hits, na.rm = TRUE),
             brt_total_false_alarms = sum(false_alarm, na.rm = TRUE),
             brt_total_correct_rej = sum(correct_rejection, na.rm = TRUE),
             brt_total_miss = sum(miss, na.rm = TRUE))
+
+#changing file path to save outputs
+setwd("Z:/Biobank_Vascular/Windows/Gorilla_Mark_1123/Clean_WEX_data_1123")
+
+#exporting tables
+#"-_ps" = full data downloaded from gorilla
+write.csv(visual_ps, file = "visual_ps_full_data.csv")
+write.csv(semantic_ps, file = "semantic_ps_full_data.csv")
+write.csv(motor_ps, file = "motor_ps_full_data.csv")
+write.csv(basic_rt, file = "basic_rt_full_data.csv")
+
+#"-_exp" = relevant experimental data - mainly used to double check whether 'correct' 
+#answers were actually correct and whether or not participants completed the tasks, 
+#main cleaning edits done here too - i.e. RT data deleted where screen has timed out 
+#this is trial-by-trial data, as ppt spans multiple rows
+write.csv(visual_exp, file = "visual_clean_trials.csv")
+write.csv(semantic_exp, file = "semantic_clean_trials.csv")
+write.csv(motor_exp, file = "motor_clean_trials.csv")
+write.csv(basic_rt_exp, file = "basic_rt_clean_trials.csv")
+
+#"-_sums" = one participant per row, contains trial averages and totals for each ppt. 
+write.csv(visual_sums, file = "visual_ppt_avgs.csv")
+write.csv(semantic_sums, file = "semantic_ppt_avgs.csv")
+write.csv(motor_sums, file = "motor_ppt_avgs.csv")
+write.csv(basic_rt_sums, file = "basic_rt_ppt_avgs.csv")
+
 
